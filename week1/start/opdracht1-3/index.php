e0224c9ae561e5f152206eba34dd6cd1f80c5f25
99051257 <99051257@mydavinci.nl>
2021-11-25 13:39:41 +0100

    Week 1 - opdracht 1.3

--- week1/start/opdracht1-3/index.php
+++ week1/start/opdracht1-3/index.php
@@ -3,6 +3,76 @@
 $musicAlbums =
     [
         // fill the collection with albums (also arrays)
+        [
+            "artist" => "Fleetwood Mac",
+            "album" => "Rumours",
+            "release_date" => "4-2-1977",
+            "tracks" => 11,
+            "genre" => ""
+        ],
+        [
+            "artist" => "Hootie And The Blowfish",
+            "album" => "Cracked Rear View",
+            "release_date" => "5-7-1994",
+            "tracks" => 12,
+            "genre" => ""
+        ],
+        [
+            "artist" => "Garth Brooks",
+            "album" => "Double Live",
+            "release_date" => "2-11-2007",
+            "tracks" => 2,
+            "genre" => ""
+        ],
+        [
+            "artist" => "AC/DC",
+            "album" => "Back In Black",
+            "release_date" => "25-7-1980",
+            "tracks" => 10,
+            "genre" => ""
+        ],
+        [
+            "artist" => "Pink Floyd",
+            "album" => "The Wall",
+            "release_date" => "30-11-1979",
+            "tracks" => 26,
+            "genre" => ""
+        ],
+        [
+            "artist" => "Led Zeppelin",
+            "album" => "Led Zeppelin IV (Remaster)",
+            "release_date" => "8-11-1971",
+            "tracks" => 8,
+            "genre" => ""
+        ],
+        [
+            "artist" => "Billy Joel",
+            "album" => "Greatest Hits Volume 1 & Volume 2",
+            "release_date" => "7-12-2006",
+            "tracks" => 26,
+            "genre" => ""
+        ],
+        [
+            "artist" => "Eagles",
+            "album" => "Hotel California",
+            "release_date" => "8-12-1976",
+            "tracks" => 9,
+            "genre" => ""
+        ],
+        [
+            "artist" => "Michael Jackson",
+            "album" => "Thriller",
+            "release_date" => "30-11-1982",
+            "tracks" => 9,
+            "genre" => "Hiphop"
+        ],
+        [
+            "artist" => "LP",
+            "album" => "Their Greatest Hits 1971-1975",
+            "release_date" => "29-11-2006",
+            "tracks" => 10,
+            "genre" => ""
+        ]
     ];
 ?>
 <!doctype html>
@@ -33,6 +103,16 @@ $musicAlbums =
     </tfoot>
     <tbody>
     <!--        Loop through all albums in the collection-->
+    <?php foreach ($musicAlbums as $key => $musicAlbum) { ?>
+        <tr>
+            <td><?= $key +1 ?></td>
+            <td><?= $musicAlbum['artist'] ?></td>
+            <td><?= $musicAlbum['album'] ?></td>
+            <td><?= $musicAlbum['genre'] ?></td>
+            <td><?= $musicAlbum['release_date'] ?></td>
+            <td><?= $musicAlbum['tracks'] ?></td>
+        </tr>
+    <?php } ?>
     </tbody>
 </table>
 </body>
