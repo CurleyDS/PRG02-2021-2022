01a734d164e07062148b2d116a8c74ea8b5952a9
99051257 <99051257@mydavinci.nl>
2021-12-02 10:50:41 +0100

    Week 2 - opdracht 2.3

--- week2/start/create.php
+++ week2/start/create.php
@@ -44,23 +44,40 @@
         <section>
             <h1>Create new album</h1>
             <form action="" method="post">
-                <label for="artist">Artist:</label>
-                <input type="text" id="artist" name="artist"><br><br>
-                <label for="album">Album:</label>
-                <input type="text" id="album" name="album"><br><br>
-                <label for="genre">Genre:</label>
-                <select name="genre" id="genre">
-                    <option value=""></option>    
-                    <!-- Loop through all albums in the collection -->
-                    <?php foreach ($genres as $genre) { ?>
-                        <option value="<?= $genre['value']; ?>"><?= $genre['name']; ?></option>
-                    <?php } ?>
-                </select><br><br>
-                <label for="year">Year:</label>
-                <input type="number" id="year" name="year" min="1889" max="<?= date("Y"); ?>" step="1"><br><br>
-                <label for="Tracks">Tracks:</label>
-                <input type="number" id="Tracks" name="Tracks" min="0"><br><br>
-                <input type="submit" value="Submit">
+                <div>
+                    <label for="artist" title="The name of the artist.">Artist:</label>
+                    <input type="text" id="artist" name="artist" required>
+                </div>
+                
+                <div>
+                    <label for="album" title="The name of the album.">Album:</label>
+                    <input type="text" id="album" name="album" required>
+                </div>
+                
+                <div>
+                    <label for="genre" title="The genre of the album.">Genre:</label>
+                    <select name="genre" id="genre" title="The genre of the album." required>
+                        <option value="Select the genre"></option>
+                        <!-- Loop through all albums in the collection -->
+                        <?php foreach ($genres as $genre) { ?>
+                            <option value="<?= $genre['value']; ?>"><?= $genre['name']; ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                
+                <div>
+                    <label for="year" title="The release date of the album.">Year:</label>
+                    <input type="number" id="year" name="year" min="1889" max="<?= date("Y"); ?>" step="1" required>
+                </div>
+                
+                <div>
+                    <label for="Tracks" title="The number of tracks of the album.">Tracks:</label>
+                    <input type="number" id="Tracks" name="Tracks" min="0" required>
+                </div>
+                
+                <div>
+                    <input type="submit" value="Submit">
+                </div>
             </form>
         </section>
         <div>
@@ -68,9 +85,3 @@
         </div>
 	</body>
 </html>
-
-<script>
-    $(function() {
-        $( "#datepicker" ).datepicker({dateFormat: 'yy'});
-    });​
-</script>
